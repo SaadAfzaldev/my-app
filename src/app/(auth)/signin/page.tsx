8eec9eceba8610e3c9ebe12d299f131993dced63
saadafzaldev <saaddevtec@gmail.com>
2024-11-25 21:54:27 +0530

    signup page

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,11 +1,49 @@
 "use client"
-import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { z } from "zod"
-import Link from "next/link" 
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useEffect, useState } from "react"
+import { useDebounce } from "@uidotdev/usehooks";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { SignUpSchema } from "@/src/schemas/signUpSchema"
 import axios from "axios"
 
+
 export default function Page () {
+
+  const [username, setUsername] = useState("")
+  const [usernameMessage, setUsernameMessage] = useState("")
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const debouncedUsername = useDebounce(username, 500)
+  const {toast} = useToast()
+  const router = useRouter()
+
+  const form = useForm({
+    resolver: zodResolver(SignUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      
+    },
+  })
+
+  useEffect(() => {
+
+    async function checkUsername() {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true)
+        setUsernameMessage("")
+        try {
+          const response = await axios.get(`/api/usernamecheck?username=${debouncedUsername}`)
+        }
+        catch (error) {}
+      }
+    }
+
+  },[debouncedUsername])
+
   return (
     <div>
       Page
